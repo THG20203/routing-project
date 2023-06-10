45f4aefaadeb85ae0373171d0c640f3e3e8d4849
THG20203 <tristanhgriffiths8@yahoo.com>
2023-06-10 12:44:57 +0100

    adding navbar with react router

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,10 +1,11 @@
 // define where children routes should be rendered
 import { Outlet } from "react-router-dom";
+import MainNavigation from "../components/MainNavigation";
 
 function RootLayout() {
   return (
     <>
-      <h1>Layout</h1>
+      <MainNavigation />
       <Outlet />
     </>
   );
