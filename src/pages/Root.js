3df1cc7e49646484f5ac67eb89384e3493e3cdcf
THG20203 <tristanhgriffiths8@yahoo.com>
2023-06-10 13:35:08 +0100

    main element styling changes - using index.css

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,13 +1,12 @@
 // define where children routes should be rendered
 import { Outlet } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
-import classes from "./Root.module.css";
 
 function RootLayout() {
   return (
     <>
       <MainNavigation />
-      <main className={classes.content}>
+      <main>
         <Outlet />
       </main>
     </>
