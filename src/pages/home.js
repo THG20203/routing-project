131844d07c0ce1ef2eaa32582b4c4a13dd99fc18
THG20203 <tristanhgriffiths8@yahoo.com>
2023-06-10 14:18:15 +0100

    navigate programmatically

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,15 +2,23 @@
 cause under the hood it renders anchor element, but listens for clicks on that element prevents 
 the browser default of sending an https request if the link is clicked, and instead takes a look at 
 the router definitions to update page accordingly, load appropriate content. */
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
 
 function HomePage() {
+  const navigate = useNavigate();
+
+  function navigateHandler() {
+    navigate("/products");
+  }
   return (
     <>
       <h1>My Home Page</h1>
       <p>
         Go to <Link to="/products">The list of products.</Link>
+      </p>
+      <p>
+        <button onClick={navigateHandler}>Navigate</button>
       </p>
     </>
   );
