66e9251fb26c10b734ce03896e3c277bd592e3d4
THG20203 <tristanhgriffiths8@yahoo.com>
2023-06-10 11:53:40 +0100

    implementing first route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/home";
 
 // path is part after the domain.
 
 // createBrowserRouter - provide objects -> every object respresents one route
-createBrowserRouter([
+const router = createBrowserRouter([
   // add some properties to define the characteristics like the path -> route should be activated
   // second part of property -> which component should be loaded when path = active
   /* connect to home page 'page' created -> to establish connection, add another key property
   element property -> define the element (the jsx code) that should be loaded when the path next to it
   is active */
-  { path: "/" },
-  { element: <HomePage /> },
+  { path: "/", element: <HomePage /> },
 ]);
 
 function App() {
-  return <div></div>;
+  /* RouterProvider has a special prop I must set -> router prop. Passing router const to router prop. */
+  return <RouterProvider router={router} />;
 }
 
 export default App;
