f66dbac4db9fbe56099dec4d2c51deace6d1ac4f
THG20203 <tristanhgriffiths8@yahoo.com>
2023-06-11 13:41:52 +0100

    implementation of dynamic routes

--- src/pages/productdetail.js
+++ src/pages/productdetail.js
@@ -1,5 +1,19 @@
+/* Now here want to know which product id was used so we could reach out to a backend API/ database
+  to fetch the data for that specific product, display it on the screen. */
+/* so use another tool in react */
+import { useParams } from "react-router-dom";
+
 function ProductDetailPage() {
-  return <h1>Product Details</h1>;
+  const params = useParams();
+  /* params object is a simple javascript object -> contains every dynamic path segment we defined in
+  our route definition as a property. */
+
+  return (
+    <>
+      <h1>Product Details</h1>
+      <p>{params.productId}</p>
+    </>
+  );
 }
 
 export default ProductDetailPage;
